1f1b0aed844c7184588708c880e45033abb7d6cb
CYRIL ADJEI <66763104+SonOfGod07@users.noreply.github.com>
2022-09-26 14:42:28 -0500

    Adding the Tracklisting Component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
 import { Album } from './album';
 
 @Injectable()
